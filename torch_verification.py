555797d635f5ece9ce8b622203877f811edd1cca
JavierGJ80 <cabojavo@hotmail.com>
2024-09-30 13:36:30 -0600

    update to use M1 GPU

--- torch_verification.py
+++ torch_verification.py
@@ -5,3 +5,9 @@ if torch.cuda.is_available():
     print("CUDA is available. GPU will be used for computations.")
 else:
     print("CUDA is not available. Computations will be on CPU.")
+
+# Check if MPS is available
+if torch.backends.mps.is_available():
+    print("MPS backend is available")
+else:
+    print("MPS backend is not available")
